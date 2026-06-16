60d9731bb12796de2fc7487fa50636ea0ff14085
srishtijha8448@gmail.com <srishtijha8448@gmail.com>
2026-06-16 10:16:28 +0530

    updated files

---
+++ Form Registration Exp/src/Components/Registration.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Registration = () => {
+
+  return (
+    <>
+    <div>Registration</div>
+    Name : <input type="text" />
+    </>
+  )
+}
+
+export default Registration
